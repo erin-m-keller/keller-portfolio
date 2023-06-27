f8acc37e42289d89cbc31c1666711c24b7fa91e0
Erin Keller <aestheticartist@gmail.com>
2023-06-27 13:40:33 -0400

    Updated about me with more personal information, added a small react image gallery to display some pictures

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
+import "react-image-gallery/styles/css/image-gallery.css";
 import Main from "./pages/Main";
 import { BrowserRouter } from "react-router-dom"
 
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,21 @@
 import React, { Component, useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import ImageGallery from 'react-image-gallery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDove } from '@fortawesome/free-solid-svg-icons';
 import selfImg from "../assets/images/erin.jpeg";
+import galleryImg1 from "../assets/images/gallery/1.jpg";
+import galleryImg2 from "../assets/images/gallery/2.jpg";
+import galleryImg3 from "../assets/images/gallery/3.jpg";
+import galleryImg4 from "../assets/images/gallery/4.jpg";
+import galleryImg5 from "../assets/images/gallery/5.jpg";
+import galleryImg6 from "../assets/images/gallery/6.jpg";
+import galleryImg7 from "../assets/images/gallery/7.jpg";
+import galleryImg8 from "../assets/images/gallery/8.jpg";
+import galleryImg9 from "../assets/images/gallery/9.jpg";
+import galleryImg10 from "../assets/images/gallery/10.jpg";
+import galleryImg11 from "../assets/images/gallery/11.jpg";
+import galleryImg12 from "../assets/images/gallery/12.jpg";
 
 // @ExperienceTimer function is modified from the code found on 
 // this blog post by Julia: https://dev.to/yuridevat/how-to-create-a-timer-with-react-7b9 
@@ -66,6 +79,56 @@ class AboutMe extends Component {
       enter: { opacity: 1 },
       exit: { opacity: 0 },
     };
+    const images = [
+      {
+        original: galleryImg1,
+        thumbnail: galleryImg1,
+      },
+      {
+        original: galleryImg2,
+        thumbnail: galleryImg2,
+      },
+      {
+        original: galleryImg3,
+        thumbnail: galleryImg3,
+      },
+      {
+        original: galleryImg4,
+        thumbnail: galleryImg4,
+      },
+      {
+        original: galleryImg5,
+        thumbnail: galleryImg5,
+      },
+      {
+        original: galleryImg6,
+        thumbnail: galleryImg6,
+      },
+      {
+        original: galleryImg7,
+        thumbnail: galleryImg7,
+      },
+      {
+        original: galleryImg8,
+        thumbnail: galleryImg8,
+      },
+      {
+        original: galleryImg9,
+        thumbnail: galleryImg9,
+      },
+      {
+        original: galleryImg10,
+        thumbnail: galleryImg10,
+      },
+      {
+        original: galleryImg11,
+        thumbnail: galleryImg11,
+      },
+      {
+        original: galleryImg12,
+        thumbnail: galleryImg12,
+      }
+    ];
     return (
       <motion.section
         initial="initial"
@@ -92,10 +155,14 @@ class AboutMe extends Component {
             </motion.div>
             </div>
             <ExperienceTimer />
-            <div className="aboutme-section">
-              <p>Erin Keller loves programming, solving problems, working in a team, and continuously learning new things. She's been <strong>developing Front End applications</strong> for over <strong>12 years</strong>. For the past decade she has been working for the Fortune 500 company <strong>IBM</strong>.</p>
-              <p>Erin has some educational background in programming but she is mainly <strong>self-taught</strong> in all things front end related. Erin is a <strong>passionate</strong> Front End Developer who focuses on writing <strong>clean</strong>, <strong>elegant</strong> and <strong>efficient code</strong>.</p>
+          </motion.div>
+          <motion.div variants={childElem} transition={{ duration: 0.5 }} className="aboutme-section">
+            <div className="personal">
+              <ImageGallery items={images} />
             </div>
+              <p className="personal-txt">Erin Keller loves programming, solving problems, working in a team, and continuously learning new things. She's been <strong>developing Front End applications</strong> for over <strong>12 years</strong>. For the past decade she has been working for the Fortune 500 company <strong>IBM</strong>.</p>
+              <p className="personal-txt">Erin has some educational background in programming but she is mainly <strong>self-taught</strong> in all things front end related. Erin is a <strong>passionate</strong> Front End Developer who focuses on writing <strong>clean</strong>, <strong>elegant</strong> and <strong>efficient code</strong>.</p>
+              <p className="personal-txt">Erin finds great joy in pursuing a diverse range of interests. An <strong>avid reader</strong> and <strong>music enthusiast</strong>, she finds solace in the pages of captivating books and the melodic tunes that resonate with her soul. Whether it's playing the <strong>piano</strong>, engaging in <strong>board games</strong> or <strong>video games</strong>, or exploring the enthralling world of <strong>true crime</strong> and <strong>anime</strong>, Erin embraces various forms of entertainment with enthusiasm. Additionally, her love for <strong>outdoor adventures</strong> such as <strong>camping</strong>, <strong>backpacking</strong>, <strong>hiking</strong>, and <strong>kayaking</strong> allows her to appreciate the wonders of nature and seek moments of tranquility. As a proud <strong>cat mom</strong> to two lovable boys named Yuki and Jello, and <strong>happily married</strong> to Taylor Keller, Erin's personal life is filled with love and cherished moments.</p>
           </motion.div>
         </div>
       </motion.section>
